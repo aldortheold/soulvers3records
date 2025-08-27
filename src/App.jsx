feaa5b373fdb55d02d7a29b0f067bd96b999e979
Andrew <aldortheold@gmail.com>
2025-08-27 15:14:26 +1000

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,25 @@ import Contact from './Contact';
 
 function App() {
 
+    const [darkMode, setDarkMode] = useState(parseInt(localStorage.getItem("darkMode")));
+
     const [openMenu, setOpenMenu] = useState(false);
     const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark");
+        }
         window.addEventListener("resize", () => setWidth(window.innerWidth));
         return () =>  { window.removeEventListener("resize", () => setWidth(window.innerWidth)) }
     }, []);
+
+    function changeMode() {
+        document.body.classList.toggle("dark");
+        setDarkMode(!darkMode);
+        if (darkMode) localStorage.removeItem("darkMode");
+        else localStorage.setItem("darkMode", "1");
+    }
 
     return (
         <BrowserRouter>
@@ -27,6 +39,9 @@ function App() {
                     <Link to="/facts">Fun Facts</Link>
                     <Link to="/music">Music</Link>
                     <Link to="/contact">Contact</Link>
+                    <button className="mode-switch" onClick={changeMode}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d={!darkMode ? "M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" : "M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"} /></svg>
+                    </button>
                 </nav>
                 : <nav className="mobile-nav">
                     <button
@@ -43,11 +58,14 @@ function App() {
                         <Link to="/music" onClick={() => setOpenMenu(false)}>Music</Link>
                         <Link to="/contact" onClick={() => setOpenMenu(false)}>Contact</Link>
                     </div>
+                    <button className="mode-switch" onClick={changeMode}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d={!darkMode ? "M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" : "M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"} /></svg>
+                    </button>
                 </nav>}
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="/facts" element={<FunFacts />} />
-                    <Route path="/music" element={<Music />} />
+                    <Route path="/music" element={<Music darkMode={darkMode} />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="*" element={<Home />} />
                 </Routes>
